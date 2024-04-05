709579700ada2ceb2494c8a559eb2986b9c68216
jeongetal <relaxative@naver.com>
2024-04-05 10:30:40 +0900

    20240405

---
+++ Blazor.Canvas.Model/Models/Video.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Canvas.Model.Models
+{
+    public class Video
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Company { get; set; } = string.Empty;
+        public string CreatedBy { get; set; } = string.Empty;
+        public int Id { get; set; } = 0;
+    }
+}
